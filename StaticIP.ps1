94b912e6c077e72433aed7561ba04da8be054c7f
Peter Szathmary <pet.szathmary@gmail.com>
2022-01-21 04:29:33 -0800

    moved files to single location

--- StaticIP.ps1
+++ StaticIP.ps1
@@ -1,8 +1,9 @@
+Get-NetIPConfiguration
 $ip = Read-Host "New IP Address"
 $primaryDNS = Read-Host "Set primary DNS"
 $secondaryDNS = Read-Host "Set secondary DNS"
 $numberOfAdapter = Read-Host "What adapter are you configuring"
 $interfaceIndex = Get-NetIPConfiguration | Where-Object -FilterScript {$_.InterfaceDescription.Contains($numberOfAdapter)} | Select-Object -ExpandProperty InterfaceIndex
 
-New-NetIPAddress -IPAddress $ip -PrefixLength 24 -InterfaceIndex $interfaceIndex
-Set-DnsClientServerAddress -InterfaceIndex $interfaceIndex -ServerAddresses ($primaryDNS, $secondaryDNS)
+#New-NetIPAddress -IPAddress $ip -PrefixLength 24 -InterfaceIndex $interfaceIndex
+#Set-DnsClientServerAddress -InterfaceIndex $interfaceIndex -ServerAddresses ($primaryDNS, $secondaryDNS)
