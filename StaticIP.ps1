67c2b8d118157e05419039354101f48036a238be
Peter Szathmary <pet.szathmary@gmail.com>
2022-01-21 05:15:46 -0800

    removed unnecessery variable

--- StaticIP.ps1
+++ StaticIP.ps1
@@ -2,11 +2,10 @@ Get-NetIPConfiguration
 $width = [Console]::WindowWidth
 Write-Host ("-" * $width + "`n")
 Write-Host "Please enter which interface you want to configure." -ForegroundColor Green
-$numberOfAdapter = Read-Host "Choose the interface index"
+$interfaceIndex = Read-Host "Choose the interface index"
 $ip = Read-Host "New IP Address"
 $primaryDNS = Read-Host "Set primary DNS"
 $secondaryDNS = Read-Host "Set secondary DNS"
-$interfaceIndex = Get-NetIPConfiguration | Where-Object -FilterScript {$_.InterfaceDescription.Contains($numberOfAdapter)} | Select-Object -ExpandProperty InterfaceIndex
 
 New-NetIPAddress -IPAddress $ip -PrefixLength 24 -InterfaceIndex $interfaceIndex
 Set-DnsClientServerAddress -InterfaceIndex $interfaceIndex -ServerAddresses ($primaryDNS, $secondaryDNS)
