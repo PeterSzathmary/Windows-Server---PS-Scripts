ad607b31beba71ab8a29e39c2b7278d080358fa7
Peter Szathmary <pet.szathmary@gmail.com>
2022-01-21 05:05:59 -0800

    improved StaticIP

--- StaticIP.ps1
+++ StaticIP.ps1
@@ -1,9 +1,12 @@
 Get-NetIPConfiguration
+$width = [Console]::WindowWidth
+Write-Host ("-" * $width + "`n")
+Write-Host "Please enter which interface you want to configure." -ForegroundColor Green
+$numberOfAdapter = Read-Host "Choose the interface index"
 $ip = Read-Host "New IP Address"
 $primaryDNS = Read-Host "Set primary DNS"
 $secondaryDNS = Read-Host "Set secondary DNS"
-$numberOfAdapter = Read-Host "What adapter are you configuring"
 $interfaceIndex = Get-NetIPConfiguration | Where-Object -FilterScript {$_.InterfaceDescription.Contains($numberOfAdapter)} | Select-Object -ExpandProperty InterfaceIndex
 
-#New-NetIPAddress -IPAddress $ip -PrefixLength 24 -InterfaceIndex $interfaceIndex
-#Set-DnsClientServerAddress -InterfaceIndex $interfaceIndex -ServerAddresses ($primaryDNS, $secondaryDNS)
+New-NetIPAddress -IPAddress $ip -PrefixLength 24 -InterfaceIndex $interfaceIndex
+Set-DnsClientServerAddress -InterfaceIndex $interfaceIndex -ServerAddresses ($primaryDNS, $secondaryDNS)
