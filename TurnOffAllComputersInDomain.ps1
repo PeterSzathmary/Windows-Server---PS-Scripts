65a1e2f86d5759c221b7c040e8d360b8b1dcc689
Peter Szathmary <pet.szathmary@gmail.com>
2022-02-07 19:54:35 -0800

    new scripts for adding scheduled task for turning off all computers in domain

---
+++ TurnOffAllComputersInDomain.ps1
@@ -0,0 +1,11 @@
+﻿[System.Collections.ArrayList]$computers = Get-ADComputer -Filter * -Properties * | Select-Object -ExpandProperty Name
+$domain = Get-WmiObject -Namespace root\cimv2 -Class Win32_ComputerSystem | Select-Object -ExpandProperty Domain
+Write-Host "All computers in domain: $($domain)" -ForegroundColor Green
+$computers.Remove($computers[0])
+Write-Host $computers
+
+Invoke-Command -ComputerName $computers -ScriptBlock {
+    Stop-Computer $(HOSTNAME.EXE) -Force
+}
+
+Stop-Computer $env:COMPUTERNAME -Force
